1ff24513d6fe7122ba0b85297e6ce3f4f33b86bc
m-noguera-matematicas <marconoguera@gmail.com>
2026-06-17 23:00:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-st.title("Refuerza tus habilidades de cálculo diferencial")
+st.title("Materiales de estudio y trabajo de cálculo diferencial e Integral")
 
 st.write("""
 Bienvenido.
 
 Aquí encontrarás:
+- Libros en PDF
 - Notas de clase
 - Tareas
 - Exámenes
