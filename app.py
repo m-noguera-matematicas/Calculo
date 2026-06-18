b0e33870f6bdd20c0efef570fe568b71b315bddc
m-noguera-matematicas <marconoguera@gmail.com>
2026-06-17 22:49:49 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Refuerza tus habilidades de cálculo diferencial")
+
+st.write("""
+Bienvenido.
+
+Aquí encontrarás:
+- Notas de clase
+- Tareas
+- Exámenes
+- Material complementario
+""")
